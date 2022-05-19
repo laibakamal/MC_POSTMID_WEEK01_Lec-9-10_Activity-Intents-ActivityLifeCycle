8eb8b55dd635d2f8be45a11b2190803abf82cd0f
LAIBA KAMAL <laibakamal003@gmail.com>
2022-05-20 01:31:31 +0500

    ...

--- app/src/main/java/com/example/mc_postmid_week01_lec9_10/MainActivity.java
+++ app/src/main/java/com/example/mc_postmid_week01_lec9_10/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.mc_postmid_week01_lec9_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +10,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity
 {
 
-    Button btn=findViewById(R.id.button);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button btn=findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener()
         {
             @Override
