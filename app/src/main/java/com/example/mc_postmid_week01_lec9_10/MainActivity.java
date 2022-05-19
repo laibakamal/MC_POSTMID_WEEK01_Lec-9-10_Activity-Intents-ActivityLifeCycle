fbc4413c1db5ce4bea481ebfeea529e4c2f48ce2
LAIBA KAMAL <laibakamal003@gmail.com>
2022-05-20 00:38:57 +0500

    ...

--- app/src/main/java/com/example/mc_postmid_week01_lec9_10/MainActivity.java
+++ app/src/main/java/com/example/mc_postmid_week01_lec9_10/MainActivity.java
@@ -11,35 +11,21 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity
 {
 
+    Button btn=findViewById(R.id.button);
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button btn=findViewById(R.id.button);
-
         btn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                Intent intent;
-                switch (view.getId())
-                {
-                    case R.id.buttonOpenSecodActivity:
-                        intent = new Intent(MainActivity.this, MainActivity2.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.buttonCallingIntent:
-                        Uri uri = Uri.parse("tel:+923001234567");
-                        intent = new Intent(Intent.ACTION_DIAL, uri);
-                        startActivity(intent);
-                        break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + view.getId());
-                }
+               Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+               startActivity(intent);
             }
         });
 
