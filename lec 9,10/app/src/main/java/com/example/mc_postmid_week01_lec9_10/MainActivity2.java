ad9ebf0111bf28a1e753c81436bf6c9998641f9e
LAIBA KAMAL <laibakamal003@gmail.com>
2022-06-01 23:25:16 +0500

    ...

--- lec 9,10/app/src/main/java/com/example/mc_postmid_week01_lec9_10/MainActivity2.java
+++ lec 9,10/app/src/main/java/com/example/mc_postmid_week01_lec9_10/MainActivity2.java
@@ -3,6 +3,8 @@ package com.example.mc_postmid_week01_lec9_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button btn2=findViewById(R.id.btn2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
